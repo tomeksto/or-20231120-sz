9a830455735a7d402b433f734d3c7ab3fd0cc04b
tomek <justabielanka@gmail.com>
2023-11-21 13:14:49 +0100

    Nowe dane

--- 1.2 - typ_danych.py
+++ 1.2 - typ_danych.py
@@ -1,7 +1,34 @@
-print("Radek")  # wypisz/wymaluj
-print(type("Radek"))
-lista = ['Radek', 'Tomek']
-lista_copy = lista.copy()
+while True:
+    print("To jest program typu: KALKULATOR")
+    print("Działania:")
+    print("mnozenie: *")
+    print("dzielenie: /")
+    print("dodawanie: +")
+    print("odejmowanie: -")
+    print("inny znak wyjscie z programu")
+    znak = input("Podaj typ działania: ")
 
-print(id(lista))
-print(id(lista_copy))
+    match znak:
+        case "*":
+            print("Wybrałeś mnożenie")
+        case "/":
+            print("Wybrałeś dzielenie")
+        case "+":
+            print("Wybrałeś dodawanie")
+        case "-":
+            print("Wybrałeś odejmowanie")
+        case _ :
+            print("Wybrałeś wyjście z programu")
+            break
+
+    while True:
+        liczba1 = input("Podaj pierwszą liczbę")
+        if liczba1.isnumeric():
+            break
+
+    while True:
+        liczba2 = input("Podaj drugą liczbę")
+        if liczba2.isnumeric():
+            break
+
+    wynik = liczba1 liczba2
